2fc20b2888a9885387a7bba425513af8faba171c
smadani2 <smadani2@illinois.edu>
2019-03-21 17:16:22 -0500

    a

--- my-simulations/1/run.sh
+++ my-simulations/1/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+anim=false
+printRoutes=false
 dirout="./my-simulations/1/"
-anim=true
-printRoutes=true
-for ((rndSeed=10; rndSeed<=10; rndSeed=rndSeed+1))
+for ((rndSeed=1; rndSeed<=10; rndSeed=rndSeed+1))
 do
-  for ((apStep=150; apStep<=150; apStep=apStep+10))
+  for apStep in 50 66 80 100
   do
-    gridSize=$((500/${apStep}+1))
-    echo ${gridSize}
-    clNum=$((100/${gridSize}/${gridSize}+1))
-    echo ${clNum}
-    ./waf --command-template="%s --rndSeed=${rndSeed} --gridSize=${gridSize} --clNum=${clNum} --apStep=${apStep} --totalTime=100 --anim=${anim} --printRoutes=${printRouts} --datarate=500Kbps --dirout=${dirout}" --run=mesh-aodv-1 &> ${dirout}aodv.result.${apStep}.${rndSeed}.txt
+    echo ${apStep} ${rndSeed}
+    gridSize=$((400/${apStep}))
+    clNum=$((200/${gridSize}/${gridSize}))
+    ./waf --command-template="%s --rndSeed=${rndSeed} --gridSize=${gridSize} --clNum=${clNum} --apStep=${apStep} --totalTime=60 --anim=${anim} --printRoutes=${printRouts} --datarate=256Kbps --dirout=${dirout}" --run=mesh-aodv-1 &> ${dirout}aodv.result.${apStep}.${rndSeed}.txt
   done
 done
