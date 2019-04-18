f485b9724d60d5ed965c90009beb0c6f45d69ce0
smadani2 <smadani2@illinois.edu>
2019-04-18 16:28:44 -0500

    a

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ do
     for ((an=3; an<=7; an=an+2))
     do
       echo ${an} ${aa} ${rr}
-      ./waf --command-template="%s --rndSeed=${rr} --gridSize=${an} --clNum=4 --apStep=${aa} --clStep=20 --totalTime=60 --startTime=1 --monitorInterval=0.5 --printRoutes=false --aptx=false --datarate=5Mbps" --run=mesh-aodv-2 &> output-aodv/aodv.result.${an}.${aa}.${rr}.2
+      ./waf --command-template="%s --rndSeed=${rr} --gridSize=${an} --clNum=4 --apStep=${aa} --clStep=20 --totalTime=60 --startTime=1 --monitorInterval=0.5 --printRoutes=false --aptx=false --datarate=5Mbps" --run=mesh-aodv-3 &> output-aodv/aodv.result.${an}.${aa}.${rr}.2
     done
   done
 done
