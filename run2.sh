8bdadb0d851532bb305e4a54f9f362289d7b1955
smadani2 <smadani2@illinois.edu>
2019-04-19 12:04:04 -0500

    some real simulations

--- run2.sh
+++ run2.sh
@@ -9,7 +9,7 @@ do
     for ((dd=1; dd<=5; dd=dd+1))
     do
       echo ${ss} ${dd} ${rr}
-      ./waf --command-template="%s --rndSeed=${rr} --apNum=27 --aptx=true --gateways=3 --datarate=${dd}Mbps --locationFile=location.txt --scale=${ss} --totalTime=60" --run=mesh-aodv-2 &> my-simulations/2/aodv.result.${ss}.${dd}.${rr}
+      ./waf --command-template="%s --rndSeed=${rr} --apNum=27 --aptx=true --gateways=3 --datarate=${dd}Mbps --locationFile=location.txt --scale=${ss} --totalTime=60" --run=mesh-aodv-3 &> my-simulations/2/aodv.result.${ss}.${dd}.${rr}
     done
   done
 done
