62aa3e1c8c205354658daada440059a6a1dc2640
smadani2 <smadani2@illinois.edu>
2019-04-26 03:31:22 -0500

    1 round

--- myjob5-1.sh
+++ myjob5-1.sh
@@ -3,22 +3,22 @@
 ./waf
 
 temp=0
-dirin="my-simulations/1_throughput/input/"
-dirout="my-simulations/1_throughput/output/olsr-udp/"
+dirin="my-simulations/2_throughput/input/"
+dirout="my-simulations/2_throughput/output/olsr-udp/"
 rrstart=10001
-rrend=10005
+rrend=10010
 
-for ((aa=10; aa<=25; aa=aa+1))
+for ((rr=${rrstart}; rr<=${rrend}; rr=rr+1))
 do
-  for ((dd=9; dd<=10; dd=dd+1))
+  for ((aa=10; aa<=25; aa=aa+1))
   do
-    for ((gg=1; gg<=3; gg=gg+1))
+    for ((dd=9; dd<=10; dd=dd+1))
     do
-      for ((rr=${rrstart}; rr<=${rrend}; rr=rr+1))
+      for ((gg=1; gg<=3; gg=gg+1))
       do
         if [ ! -f "${dirout}mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt" ]; then
           echo mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt
-          ./waf --run "mesh-loc-2 --apNum=${aa} --clNum=0 --aptx=true --rndSeed=${rr} -totalTime=40 --locationFile=${dirin}location_400_0_${aa}_${dd}.txt --gateways=${gg} --route=olsr" &> "${dirout}mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt"
+          ./waf --run "mesh-loc-3 --apNum=${aa} --clNum=0 --aptx=true --rndSeed=${rr} -totalTime=40 --locationFile=${dirin}location_400_0_${aa}_${dd}.txt --gateways=${gg} --route=olsr" &> "${dirout}mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt"
         fi
       done
     done
