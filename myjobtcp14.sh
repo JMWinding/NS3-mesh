6ad9620c0873a75199a192a8ffba27e2d53435be
smadani2 <smadani2@illinois.edu>
2019-06-03 19:45:30 -0500

    aodv-tcp results

---
+++ myjobtcp14.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+./waf
+
+temp=0
+dirin="my-simulations/2_throughput/input/"
+dirout="my-simulations/2_throughput/output/aodv-tcp/"
+rrstart=10014
+rrend=10016
+
+for ((rr=${rrstart}; rr<=${rrend}; rr=rr+16))
+do
+  for ((aa=10; aa<=25; aa=aa+1))
+  do
+    for ((dd=1; dd<=10; dd=dd+1))
+    do
+      for ((gg=1; gg<=3; gg=gg+1))
+      do
+        if [ ! -f "${dirout}mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt" ]; then
+          echo mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt
+          ./waf --run "mesh-loc-1 --apNum=${aa} --clNum=0 --aptx=true --rndSeed=${rr} -totalTime=120 --locationFile=${dirin}location_400_0_${aa}_${dd}.txt --scale=80 --gateways=${gg} --app=tcp --datarate=5e5" &> "${dirout}mesh_400_0_${aa}_${dd}_${gg}_${rr}.txt"
+        fi
+      done
+    done
+  done
+done
