7150bcbc18fc4f4e645bd530895d1e62145d7b08
jw27 <jw27@illinois.edu>
2019-07-13 21:52:31 -0500

    a

--- myjob-olsr-udp-error.sh
+++ myjob-olsr-udp-error.sh
@@ -18,7 +18,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0-1 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
@@ -46,7 +46,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
@@ -74,7 +74,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0-1 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
@@ -102,7 +102,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
@@ -130,7 +130,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0-1 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
@@ -158,7 +158,7 @@ do
     for ((bb=1; bb<=aa; bb=bb+1))
     do
         cc=$((aa * bb))
-#        [ ! ${cc} -le 40 ] && continue
+        [ ! ${cc} -gt 40 ] && continue
         if [ ! -f "${dirout}/mesh_${aa}_${bb}_${rr}.xmp" ]; then
           echo mesh_${aa}_${bb}_${rr}.txt
           ./waf --run "mesh-loc-0 --gridSize=${aa} --apNum=${cc} --apStep=60 --aptx=true --gateways=1 --rateControl=${ratecontrol} --rndSeed=${rr} --totalTime=180 --flowout=${dirout}/mesh_${aa}_${bb}_${rr}.xmp --datarate=2e6" &> "${dirout}/mesh_${aa}_${bb}_${rr}.txt"
